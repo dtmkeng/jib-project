0d96991b2184782dfb98ed762ede8ef5acbb2a10
dtmkeng <phumai@odds.team>
2020-08-08 11:21:22 +0700

    call api with module request

--- safety/covid/tests/tests_views.py
+++ safety/covid/tests/tests_views.py
@@ -1,7 +1,14 @@
 from django.test import TestCase
+from unittest.mock import patch
 
 # Create your tests here.
 class TestCovidView(TestCase):
-    def test_view_should_be_accessible(self):
+    @patch('covid.views.requests.get')
+    def test_view_should_be_accessible(self, _):
         responce = self.client.get('/covid/')
         self.assertEqual(responce.status_code, 200)
+    
+    @patch('covid.views.requests.get')
+    def test_view_should_call_covid_api(self, mock):
+        self.client.get('/covid/')
+        mock.assert_called_once_with('https://covid19.th-stat.com/api/open/today')
--- safety/covid/views.py
+++ safety/covid/views.py
@@ -1,7 +1,9 @@
 from django.views import View
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 class CovidView(View):
     def get(self, request):
+        requests.get('https://covid19.th-stat.com/api/open/today')
         return HttpResponse()
