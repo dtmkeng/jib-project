2954934ca239da41cf4c672a9fb8da55e762d32c
dtmkeng <phumai@odds.team>
2020-08-08 14:37:59 +0700

    test serializer

---
+++ safety/workers/tests/test_serializers.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+from ..serializers import WorkerSerializer
+from ..models import Worker
+
+
+class TestSerializer(TestCase):
+    def test_serializer_should_return_correct_serialized_data(self):
+        first_name = 'Keng'
+        last_name = 'Mark'
+        is_available = True
+        primary_phone = '081-669-777x'
+        secondary_phone = '092-009-000x'
+        address = 'Geeky Bass All Start'
+        
+        # When
+        worker = Worker.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            is_available=is_available,
+            primary_phone=primary_phone,
+            secondary_phone=secondary_phone,
+            address=address,
+        )
+        
+        serializer = WorkerSerializer(worker)
+        
+        expected = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'is_available': is_available,
+            'primary_phone': primary_phone,
+            'secondary_phone': secondary_phone,
+            'address': address,
+        }
+        
+        assert serializer.data == expected
+    
--- safety/workers/views.py
+++ safety/workers/views.py
@@ -3,19 +3,10 @@
 # from django.views import View
 # from django.http import HttpResponse
 from .models import Worker
+from .serializers import WorkerSerializer
 # from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework import serializers
 from rest_framework.views import APIView
-
-
-class WorkerSerializer(serializers.Serializer):
-    first_name = serializers.CharField()
-    last_name = serializers.CharField()
-    is_available = serializers.BooleanField()
-    primary_phone = serializers.CharField()
-    secondary_phone = serializers.CharField()
-    address = serializers.CharField()
 
 
 # class base view
