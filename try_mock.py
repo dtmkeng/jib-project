c8a65f2906e8236be3376647b52536056c95f18a
dtmkeng <phumai@odds.team>
2020-08-08 13:15:07 +0700

    fix lint

--- try_mock.py
+++ try_mock.py
@@ -1,24 +1,23 @@
 import unittest
 from unittest.mock import patch
-
 import getran
 
+
 class TestRandom(unittest.TestCase):
-    
     @patch('__main__.getran.random.randint')
     def test_it_should_call_raninit_with_1_and_10(self, mock):
-        #call
+        # call
         getran.get_random_number_plus_one()
-        ## assert here
+        # assert here
         mock.assert_called_once_with(1, 10)
-        
+
     @patch('getran.random.randint')
     def test_it_should_get_4_when_random_get_3(self, mock):
-        #mock
+        # mock
         mock.return_value = 3
-        #call
+        # call
         result = getran.get_random_number_plus_one()
-        ## assert here
+        # assert here
         self.assertEqual(result, 4)
 
     def test_it_should_get_3(self):
@@ -27,5 +26,6 @@ class TestRandom(unittest.TestCase):
             if result == 3:
                 break
         self.assertEqual(result, 3)
+
 
 unittest.main()
